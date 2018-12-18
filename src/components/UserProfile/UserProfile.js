7aff1b46f4d5b2c21b4b8356b6e2dae6cb1139f1
Matt Hansen <hans1312@gmail.com>
2018-12-18 15:22:17 -0600

    initial commit

--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -4,6 +4,47 @@ import { connect } from 'react-redux';
 // Material-UI
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+
+const styles = {
+  container: {
+    margin: 25
+  },
+  textField: {
+    width: 400,
+    marginBottom: 15
+  },
+  text: {
+    textAlign: 'center',
+    width: 330,
+    margin: 'auto',
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontSize: 18
+  },
+  form: {
+    textAlign: 'center',
+    padding: 50
+  },
+  header: {
+    textAlign: 'center',
+    marginBottom: 25
+  },
+  button: {
+    width: 200,
+    marginTop: 15
+  },
+  div: {
+    textAlign: 'center',
+    width: 330,
+    margin: 'auto',
+    paddingTop: 100,
+    paddingBottom: 10,
+    fontSize: 18
+  }
+};
 
 class UserProfile extends Component {
 
@@ -17,16 +58,21 @@ class UserProfile extends Component {
    return (
      <div>
        <Typography variant="h2" className={classes.header}>User Profile</Typography>
+       <form>
+         <div className={classes.text}>
+         <Typography className={classes.text}>Jane</Typography>
+          </div>
+       </form>
      </div>
    );
  }
 };
 
-const styles = {
- header: {
+// const styles = {
+//  header: {
 
- }
-};
+//  }
+// };
 
 const mapReduxStateToProps = (reduxState) => ({
  reduxState

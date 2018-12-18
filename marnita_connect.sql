7cafb33551bb777e85467a1ce74fceb22bf1f95e
Jake Wylie Alexios <jwalexios@gmail.com>
2018-12-18 10:07:46 -0600

    update sql with threesixty, replacing all 360

--- marnita_connect.sql
+++ marnita_connect.sql
@@ -14,7 +14,7 @@ CREATE TABLE "access" (
     "access_type" VARCHAR(80)
 );
 
-CREATE TABLE "360" (
+CREATE TABLE "threesixty" (
     "id" SERIAL PRIMARY KEY,
     "name" VARCHAR(80),
     "date" DATE,
@@ -26,7 +26,7 @@ CREATE TABLE "360" (
 
 CREATE TABLE "person" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "email" VARCHAR(80) UNIQUE,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL,
@@ -37,15 +37,15 @@ CREATE TABLE "person" (
     "date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE "360_user" (
+CREATE TABLE "threesixty_user" (
     "id" SERIAL PRIMARY KEY,
     "user_id" INT REFERENCES "person",
-    "360_id" INT REFERENCES "360"
+    "threesixty_id" INT REFERENCES "threesixty"
 );
 
 CREATE TABLE "goals" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "description" VARCHAR(255),
     "desired" INT,
     "delivered" INT,
@@ -56,7 +56,7 @@ CREATE TABLE "goals" (
 
 CREATE TABLE "dashboard" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "row_title" VARCHAR(80),
     "row_info" VARCHAR(255),
     "private" BOOLEAN DEFAULT FALSE
@@ -64,14 +64,14 @@ CREATE TABLE "dashboard" (
 
 CREATE TABLE "analysis_recommendation" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "findings" VARCHAR(255),
     "recommendations" VARCHAR(255)
 );
 
 CREATE TABLE "demographic" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "ethnicity" VARCHAR(80),
     "passion" VARCHAR(255),
     "profession" VARCHAR(80),
@@ -86,9 +86,9 @@ CREATE TABLE "demographic" (
     "education" BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE "360_reports" (
+CREATE TABLE "threesixty_reports" (
     "id" SERIAL PRIMARY KEY,
-    "360_id" INT REFERENCES "360",
+    "threesixty_id" INT REFERENCES "threesixty",
     "demographic" VARCHAR(80),
     "summary" VARCHAR(255),
     "methodology" VARCHAR(255),
@@ -98,7 +98,7 @@ CREATE TABLE "360_reports" (
 
 CREATE TABLE "oral_report" (
     "id" SERIAL PRIMARY KEY,
-    "360_reports_id" INT REFERENCES "360_reports",
+    "threesixty_reports_id" INT REFERENCES "threesixty_reports",
     "group_num" INT,
     "response" VARCHAR(500)
 );
@@ -106,7 +106,7 @@ CREATE TABLE "oral_report" (
 CREATE TABLE "questions" (
     "id" SERIAL PRIMARY KEY,
     "set_id" INT,
-    "360_reports_id" INT REFERENCES "360_reports",
+    "threesixty_reports_id" INT REFERENCES "threesixty_reports",
     "question" VARCHAR(255),
     "blurb" VARCHAR(255),
     "breakdown" VARCHAR(255)
@@ -114,7 +114,7 @@ CREATE TABLE "questions" (
 
 CREATE TABLE "circle_share" (
     "id" SERIAL PRIMARY KEY,
-    "360_reports_id" INT REFERENCES "360_reports",
+    "threesixty_reports_id" INT REFERENCES "threesixty_reports",
     "question" VARCHAR(255),
     "responses" VARCHAR(255)
 );

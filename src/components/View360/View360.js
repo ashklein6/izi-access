0e214a0fdf07b786bf73eae089bcbc685fd98649
Joe Evert <joeevert@gmail.com>
2018-12-17 20:49:27 -0600

    added scroll to main viewing area

--- src/components/View360/View360.js
+++ src/components/View360/View360.js
@@ -18,78 +18,163 @@ const styles = {
     backgroundColor: '#ccc',
     textAlign: 'center',
   },
-  header: {
+  report: {
+    height: '100vh',
+  },
+  mainInfo: {
     textAlign: 'center',
-    margin: 25
+    height: '35vh'
+  },
+  header: {
+    margin: '50px 0px 25px 0px'
   },
   subHeader: {
-    textAlign: 'center',
+    marginBottom: '15px'
   },
   dashboardBtn: {
     margin: '15px 0px',
     justifyContent: 'center',
   },
   list: {
-    paddingTop: 0
+    paddingTop: '0px'
+  },
+  middleDivider: {
+    margin: '25px auto',
+    width: '90%'
+  },
+  content: {
+    textAlign: 'left',
+    margin: 'auto',
+    width: '85%',
+    height: '55vh',
+    overflow: 'auto'
+  },
+  paragraph:{
+    margin: '0px 5px 50px 5px',
+    textIndent: '25px'
   }
 };
 
 class View360 extends Component {
 
- state = {
- };
+  state = {
 
-// will navigate to /manage360s
-toManage360s = (event) => {
-  event.preventDefault();
-  this.props.history.push('/manage360s');
-}
- render() {
-  const { classes } = this.props;
+  };
 
-  return (
-    <Grid container spacing={0}>
-      <Grid className={classes.sideBar} item xs={2}>
-        <Button
-          className={classes.dashboardBtn} 
-          variant="contained"
-          onClick={this.toManage360s}
-        >
-          Return to Dashboard
-        </Button>
-        <Divider />
-        <List className={classes.list}>
-          <ListItem button>
-            <Typography>Goals Assessment</Typography>
-          </ListItem>
+  // will navigate to /manage360s
+  toManage360s = (event) => {
+    event.preventDefault();
+    this.props.history.push('/manage360s');
+  }
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <Grid container spacing={0}>
+        <Grid className={classes.sideBar} item xs={2}>
+          <Button
+            className={classes.dashboardBtn} 
+            variant="contained"
+            onClick={this.toManage360s}
+          >
+            Return to Dashboard
+          </Button>
           <Divider />
-          <ListItem button>
-            <Typography>Dashboard</Typography>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <Typography>360 Report</Typography>
-          </ListItem>
-          <Divider />
-          <ListItem button>
-            <Typography>Analysis &amp; Recommendations</Typography>
-          </ListItem>
-          <Divider />
-        </List>
+          <List className={classes.list}>
+            <ListItem button>
+              <Typography>Goals Assessment</Typography>
+            </ListItem>
+            <Divider />
+            <ListItem button>
+              <Typography>Dashboard</Typography>
+            </ListItem>
+            <Divider />
+            <ListItem button>
+              <Typography>360 Report</Typography>
+            </ListItem>
+            <Divider />
+            <ListItem button>
+              <Typography>Analysis &amp; Recommendations</Typography>
+            </ListItem>
+            <Divider />
+          </List>
+        </Grid>
+        <Grid className={classes.report} item xs={10}>
+          <section className={classes.mainInfo}>
+            <Typography variant="h2" className={classes.header}>360 Name</Typography>
+            <Typography variant="h5" className={classes.subHeader}>Client</Typography>
+            <Typography variant="h5" className={classes.subHeader}>Location</Typography>
+            <Typography variant="h5" className={classes.subHeader}>Date</Typography>
+            <Divider className={classes.middleDivider} />
+          </section>
+          <section className={classes.content}>
+            <Typography variant="h5" className={classes.subHeader}>Goals Assessment</Typography>
+            <div className={classes.paragraph}>
+              <Typography>
+                Ut vel ipsum porttitor, varius nisl eget, suscipit leo. Maecenas mollis orci sagittis, vehicula ante ac, aliquet libero. Vivamus finibus finibus semper. 
+                Praesent convallis arcu sapien, quis porttitor magna dapibus ac. Suspendisse facilisis ut felis et mattis. Duis et sem ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Maecenas malesuada sem vitae urna pretium, id auctor neque vulputate. Proin sed urna ac tortor volutpat hendrerit sed in massa. Nunc varius neque eget nibh fringilla, a tincidunt nibh mollis. 
+                Aenean condimentum porta felis sed vestibulum. Donec placerat purus urna, vel consectetur libero rutrum sit amet. Vestibulum egestas sapien sed eros pellentesque, at malesuada dolor tempor. 
+                Vestibulum id libero vitae leo vehicula mattis. Etiam ante magna, rutrum at suscipit tempus, efficitur eu turpis. Sed in magna sed leo pretium rutrum eget eleifend risus.
+              </Typography>
+            </div>
+
+            <Typography variant="h5" className={classes.subHeader}>Dashboard</Typography>
+            <div className={classes.paragraph}>
+              <Typography>
+                Ut vel ipsum porttitor, varius nisl eget, suscipit leo. Maecenas mollis orci sagittis, vehicula ante ac, aliquet libero. Vivamus finibus finibus semper. 
+                Praesent convallis arcu sapien, quis porttitor magna dapibus ac. Suspendisse facilisis ut felis et mattis. Duis et sem ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Maecenas malesuada sem vitae urna pretium, id auctor neque vulputate. Proin sed urna ac tortor volutpat hendrerit sed in massa. Nunc varius neque eget nibh fringilla, a tincidunt nibh mollis. 
+                Aenean condimentum porta felis sed vestibulum. Donec placerat purus urna, vel consectetur libero rutrum sit amet. Vestibulum egestas sapien sed eros pellentesque, at malesuada dolor tempor. 
+                Vestibulum id libero vitae leo vehicula mattis. Etiam ante magna, rutrum at suscipit tempus, efficitur eu turpis. Sed in magna sed leo pretium rutrum eget eleifend risus.
+              </Typography>
+            </div>
+
+            <Typography variant="h5" className={classes.subHeader}>360 Report</Typography>
+            <div className={classes.paragraph}>
+              <Typography>
+                Nullam sodales leo nec viverra semper. Fusce suscipit porttitor molestie. Phasellus blandit ut diam vel tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Aenean molestie dui quis lectus tempor, a laoreet mi blandit. Curabitur quis elit mauris. Ut sollicitudin cursus volutpat. Donec sodales consectetur tincidunt. 
+                Duis posuere auctor risus. Donec sit amet ante urna.
+                <br/>
+                <br/>
+                Ut vel ipsum porttitor, varius nisl eget, suscipit leo. Maecenas mollis orci sagittis, vehicula ante ac, aliquet libero. Vivamus finibus finibus semper. 
+                Praesent convallis arcu sapien, quis porttitor magna dapibus ac. Suspendisse facilisis ut felis et mattis. Duis et sem ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Maecenas malesuada sem vitae urna pretium, id auctor neque vulputate. Proin sed urna ac tortor volutpat hendrerit sed in massa. Nunc varius neque eget nibh fringilla, a tincidunt nibh mollis. 
+                Aenean condimentum porta felis sed vestibulum. Donec placerat purus urna, vel consectetur libero rutrum sit amet. Vestibulum egestas sapien sed eros pellentesque, at malesuada dolor tempor. 
+                Vestibulum id libero vitae leo vehicula mattis. Etiam ante magna, rutrum at suscipit tempus, efficitur eu turpis. Sed in magna sed leo pretium rutrum eget eleifend risus.
+                <br/>
+                <br/>
+                Nullam sodales leo nec viverra semper. Fusce suscipit porttitor molestie. Phasellus blandit ut diam vel tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Aenean molestie dui quis lectus tempor, a laoreet mi blandit. Curabitur quis elit mauris. Ut sollicitudin cursus volutpat. Donec sodales consectetur tincidunt. 
+                Duis posuere auctor risus. Donec sit amet ante urna.
+              </Typography>
+            </div>
+
+            <Typography variant="h5" className={classes.subHeader}>Analysis &amp; Recommendations</Typography>
+            <div className={classes.paragraph}>
+              <Typography>
+                Ut vel ipsum porttitor, varius nisl eget, suscipit leo. Maecenas mollis orci sagittis, vehicula ante ac, aliquet libero. Vivamus finibus finibus semper. 
+                Praesent convallis arcu sapien, quis porttitor magna dapibus ac. Suspendisse facilisis ut felis et mattis. Duis et sem ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+                Maecenas malesuada sem vitae urna pretium, id auctor neque vulputate. Proin sed urna ac tortor volutpat hendrerit sed in massa. Nunc varius neque eget nibh fringilla, a tincidunt nibh mollis. 
+                Aenean condimentum porta felis sed vestibulum. Donec placerat purus urna, vel consectetur libero rutrum sit amet. Vestibulum egestas sapien sed eros pellentesque, at malesuada dolor tempor. 
+                Vestibulum id libero vitae leo vehicula mattis. Etiam ante magna, rutrum at suscipit tempus, efficitur eu turpis. Sed in magna sed leo pretium rutrum eget eleifend risus.
+                <br/>
+                <br/>
+                Nullam sodales leo nec viverra semper. Fusce suscipit porttitor molestie. Phasellus blandit ut diam vel tempor. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Aenean molestie dui quis lectus tempor, a laoreet mi blandit. Curabitur quis elit mauris. Ut sollicitudin cursus volutpat. Donec sodales consectetur tincidunt. 
+                Duis posuere auctor risus. Donec sit amet ante urna.
+              </Typography>
+            </div>
+          </section>
+        </Grid>
       </Grid>
-      <Grid item xs={10}>
-        <Typography variant="h2" className={classes.header}>View 360</Typography>
-        <Typography variant="h5" className={classes.subHeader}>Client</Typography>
-        <Typography variant="h5" className={classes.subHeader}>Location</Typography>
-        <Typography variant="h5" className={classes.subHeader}>Date</Typography>
-      </Grid>
-    </Grid>
-   );
- }
+    );
+  }
 };
 
 const mapReduxStateToProps = (reduxState) => ({
- reduxState
+  reduxState
 });
 
 export default connect(mapReduxStateToProps)(withStyles(styles)(View360));

e5842a2d31fba42c59f4e58b38700a1ae493828d
Joe Evert <joeevert@gmail.com>
2018-12-18 19:22:15 -0600

    search inputs in place

--- src/components/ManageUsers/ManageUsers.js
+++ src/components/ManageUsers/ManageUsers.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PendingClientRequests from './ExpansionPanels/PendingClientRequests';
+import Users from './ExpansionPanels/Users';
 import DeactivatedUsers from './ExpansionPanels/DeactivatedUsers';
 
 // Material-UI
 import { withStyles } from '@material-ui/core/styles';
 import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 
 class ManageUsers extends Component {
 
   state = {
   };
 
+  returnToDash = () => {
+    this.props.history.push('/dashboard');
+  };
+
   render() {
     const { classes } = this.props;
   return (
     <div className={classes.root}>
+      <Button variant="contained" onClick={this.returnToDash}>Return to Dashboard</Button>
+
       <PendingClientRequests />
+      <Users />
       <DeactivatedUsers />
     </div>
     );

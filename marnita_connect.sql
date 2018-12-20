3e72b1f111238bcd9f6a993f1b68626bd55ae3c0
Joe Evert <joeevert@gmail.com>
2018-12-20 14:18:05 -0600

    updated sql tables file

--- marnita_connect.sql
+++ marnita_connect.sql
@@ -1,6 +1,27 @@
 CREATE TABLE "access" (
     "id" SERIAL PRIMARY KEY,
-    "category" VARCHAR(256)
+    "access_level" INT,
+    "access_type" VARCHAR(255)
+);
+
+CREATE TABLE "person" (
+    "id" SERIAL PRIMARY KEY,
+    "email" VARCHAR(255) UNIQUE,
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (1000) NOT NULL,
+    "firstname" VARCHAR(255),
+    "lastname" VARCHAR(255),
+    "access_id" INT REFERENCES "access",
+    "notes" VARCHAR(2560),
+    "date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE "client_request" (
+    "id" SERIAL PRIMARY KEY,
+    "person_id" INT REFERENCES "person",
+    "name" VARCHAR (256),
+    "date" DATE DEFAULT ('1900-01-01 00:00:00.000'),
+    "date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE "izi_categories" (
@@ -19,6 +40,32 @@ CREATE TABLE "threesixty" (
     "published_status" BOOLEAN DEFAULT FALSE
 );
 
+CREATE TABLE "analysis_recommendation" (
+    "id" SERIAL PRIMARY KEY,
+    "threesixty_id" INT REFERENCES "threesixty",
+    "findings" VARCHAR(25600),
+    "recommendations" VARCHAR(25600)
+);
+
+CREATE TABLE "dashboard" (
+    "id" SERIAL PRIMARY KEY,
+    "threesixty_id" INT REFERENCES "threesixty",
+    "row_title" VARCHAR(25600),
+    "row_info" VARCHAR(25600),
+    "private" BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE "goals" (
+    "id" SERIAL PRIMARY KEY,
+    "threesixty_id" INT REFERENCES "threesixty",
+    "description" VARCHAR(25600),
+    "desired" INT,
+    "delivered" INT,
+    "difference" INT,
+    "percent" INT,
+    "comments" VARCHAR(25600)
+);
+
 CREATE TABLE "threesixty_reports" (
     "id" SERIAL PRIMARY KEY,
     "threesixty_id" INT REFERENCES "threesixty",
@@ -27,46 +74,36 @@ CREATE TABLE "threesixty_reports" (
     "methodology" VARCHAR(25600)
 );
 
-CREATE TABLE "analysis_recommendation" (
-    "id" SERIAL PRIMARY KEY,
-    "threesixty_id" INT REFERENCES "threesixty",
-    "findings" VARCHAR(25600),
-    "recommendations" VARCHAR(25600)
-);
-
-CREATE TABLE "person" (
-    "id" SERIAL PRIMARY KEY,
-    "email" VARCHAR(255) UNIQUE,
-    "username" VARCHAR (255) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL,
-    "firstname" VARCHAR(255),
-    "lastname" VARCHAR(255),
-    "access_id" INT REFERENCES "access",
-    "notes" VARCHAR(2560),
-    "date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE "circle_share" (
+CREATE TABLE "oral_report" (
     "id" SERIAL PRIMARY KEY,
     "threesixty_reports_id" INT REFERENCES "threesixty_reports",
-    "question" VARCHAR(25600),
-    "responses" VARCHAR(25600)
+    "group_num" INT,
+    "response" VARCHAR(25600)
 );
 
-CREATE TABLE "client_request" (
-    "id" SERIAL PRIMARY KEY,
-    "person_id" INT REFERENCES "person",
-    "name" VARCHAR (256),
-    "date" DATE DEFAULT ('1900-01-01 00:00:00.000'),
-    "date_added" TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
-
-CREATE TABLE "threesixty_reports" (
+CREATE TABLE "response_category" (
     "id" SERIAL PRIMARY KEY,
-    "threesixty_id" INT REFERENCES "threesixty",
-    "row_title" VARCHAR(25600),
-    "row_info" VARCHAR(25600),
-    "private" BOOLEAN DEFAULT FALSE
+    "description" VARCHAR(255)
+);
+    
+CREATE TABLE "question_set" (
+    "id" SERIAL PRIMARY KEY,
+    "threesixty_id" INT REFERENCES "threesixty_reports",
+    "set_title" VARCHAR(25600),
+    "breakdown" VARCHAR(25600)
+);
+
+CREATE TABLE "questions" (
+    "id" SERIAL PRIMARY KEY,
+    "set_id" INT REFERENCES "question_set",
+    "question" VARCHAR(25600)
+);
+
+CREATE TABLE "response" (
+    "id" SERIAL PRIMARY KEY,
+    "question_id" INT REFERENCES "questions",
+    "response" VARCHAR(25600),
+    "category_id" INT REFERENCES "response_category"
 );
 
 CREATE TABLE "demographic" (
@@ -86,44 +123,11 @@ CREATE TABLE "demographic" (
     "education" BOOLEAN DEFAULT FALSE
 );
 
-CREATE TABLE "goals" (
-    "id" SERIAL PRIMARY KEY,
-    "threesixty_id" INT REFERENCES "threesixty",
-    "description" VARCHAR(25600),
-    "desired" INT,
-    "delivered" INT,
-    "difference" INT,
-    "percent" INT,
-    "comments" VARCHAR(25600)
-);
-
-CREATE TABLE "response" (
+CREATE TABLE "circle_share" (
     "id" SERIAL PRIMARY KEY,
     "threesixty_reports_id" INT REFERENCES "threesixty_reports",
-    "group_num" INT,
-    "response" VARCHAR(25600)
-);
-
-CREATE TABLE "demographic" (
-    "id" SERIAL PRIMARY KEY,
-    "set_id" INT,
-    "threesixty_reports_id" INT REFERENCES "threesixty_reports",
-    "question" VARCHAR(512),
-    "blurb" VARCHAR(25600),
-    "breakdown" VARCHAR(25600)
-);
-
-CREATE TABLE "response_category" (
-    "id" SERIAL PRIMARY KEY,
-    "description" VARCHAR(255)
-);
-
-CREATE TABLE "threesixty_user" (
-    "id" SERIAL PRIMARY KEY,
-    "question_set_id" INT REFERENCES "questions",
-    "question_id" INT REFERENCES "questions",
-    "response" VARCHAR(25600),
-    "category_id" INT REFERENCES "response_category"
+    "question" VARCHAR(25600),
+    "responses" VARCHAR(25600)
 );
 
 CREATE TABLE "threesixty_user" (

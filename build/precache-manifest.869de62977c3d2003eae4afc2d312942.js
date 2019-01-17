8a01f637e25fd594516e8b6b70f3944e30a0be80
Jonathan Reckinger <jpreckinger@gmail.com>
2019-01-16 18:30:17 -0600

    prep for deployment

---
+++ build/precache-manifest.869de62977c3d2003eae4afc2d312942.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cf47a3036473313ad4ecfaf7afbd4490",
+    "url": "/static/media/marnita_logo.cf47a303.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c2ca3bea117135d6d066",
+    "url": "/static/js/main.c2ca3bea.chunk.js"
+  },
+  {
+    "revision": "072c0a5226be83499f85",
+    "url": "/static/js/1.072c0a52.chunk.js"
+  },
+  {
+    "revision": "c2ca3bea117135d6d066",
+    "url": "/static/css/main.2ab48292.chunk.css"
+  },
+  {
+    "revision": "072c0a5226be83499f85",
+    "url": "/static/css/1.4f81526c.chunk.css"
+  },
+  {
+    "revision": "f52918e8797ecaadd193de11bc0e9d41",
+    "url": "/index.html"
+  }
+];

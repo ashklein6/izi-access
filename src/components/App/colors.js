2b534ae85a4e33d355fef23677f0e174cbc9cb1c
ashklein6 <ashklein6@gmail.com>
2019-02-13 10:22:52 -0600

    added default values to App/colors.js

--- src/components/App/colors.js
+++ src/components/App/colors.js
@@ -1,15 +1,17 @@
 let root = document.querySelector(':root');
 const colors = {
-  purple: window.getComputedStyle(root).getPropertyValue('--main-purple'),
-  purpleHover: window.getComputedStyle(root).getPropertyValue('--main-purple-hover'),
-  red: window.getComputedStyle(root).getPropertyValue('--main-red'),
-  redHover: window.getComputedStyle(root).getPropertyValue('--main-red-hover'),
-  pink: window.getComputedStyle(root).getPropertyValue('--main-pink'),
-  pinkHover: window.getComputedStyle(root).getPropertyValue('--main-pink-hover'),
-  orange: window.getComputedStyle(root).getPropertyValue('--main-orange'),
-  orangeHover: window.getComputedStyle(root).getPropertyValue('--main-orange-hover'),
-  lightGrey: window.getComputedStyle(root).getPropertyValue('--main-light-grey'),
-  grey: window.getComputedStyle(root).getPropertyValue('--main-grey'),
+  purple: window.getComputedStyle(root).getPropertyValue('--main-purple') || '#7d52a1',
+  purpleHover: window.getComputedStyle(root).getPropertyValue('--main-purple-hover') || '#633589',
+  red: window.getComputedStyle(root).getPropertyValue('--main-red') || '#d71f2e',
+  redHover: window.getComputedStyle(root).getPropertyValue('--main-red-hover') || '#aa0f18',
+  pink: window.getComputedStyle(root).getPropertyValue('--main-pink') || '#ec008c',
+  pinkHover: window.getComputedStyle(root).getPropertyValue('--main-pink-hover') || '#de0082',
+  orange: window.getComputedStyle(root).getPropertyValue('--main-orange') || '#eb983f',
+  orangeHover: window.getComputedStyle(root).getPropertyValue('--main-orange-hover') || '#d17818',
+  lightGrey: window.getComputedStyle(root).getPropertyValue('--main-light-grey') || '#f2f2f2',
+  grey: window.getComputedStyle(root).getPropertyValue('--main-grey') || '#dddddd',
 };
+
+console.log('colors:', colors);
 
 export default colors;
